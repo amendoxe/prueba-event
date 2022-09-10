436d64c3838c9efc58e92c8b6ff7454bfff171a1
amendoxe <arturo_mendoza@outlook.com>
2022-09-10 18:55:53 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const encabezado = document.querySelector(".title-text");
+const parrafo = document.querySelector(".paragraph-text");
+document.querySelector(".button-container").addEventListener("click", function (event) {
+	// alert(`you clicked button number ${event.target.innerText}`);
+	encabezado.innerText = "A button has been pressed";
+	parrafo.innerText = `El contenido del botón es: ${event.target.innerText}`;
+	console.log(event.target);
+});
