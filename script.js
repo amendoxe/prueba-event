8703ce78e9b821c8886e1bc4480bac10b81cf231
amendoxe <arturo_mendoza@outlook.com>
2022-09-10 18:59:24 -0500

    Add files and ready for some testing

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const encabezado = document.querySelector(".title-text");
 const parrafo = document.querySelector(".paragraph-text");
 document.querySelector(".button-container").addEventListener("click", function (event) {
 	// alert(`you clicked button number ${event.target.innerText}`);
-	encabezado.innerText = "A button has been pressed";
-	parrafo.innerText = `El contenido del botón es: ${event.target.innerText}`;
 	console.log(event.target);
+	if (event.target.type == "submit") {
+		encabezado.innerText = "A button has been pressed";
+		parrafo.innerText = `El contenido del botón es: ${event.target.innerText}`;
+	}
 });
